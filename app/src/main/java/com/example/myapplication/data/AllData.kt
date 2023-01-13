89a31e83a937955b20d8b5ebf1a33b3c2c3553ab
stev3j <mygajeong@gmail.com>
2023-01-13 19:11:09 +0900

    add: Home Screen WebToons

--- app/src/main/java/com/example/myapplication/data/AllData.kt
+++ app/src/main/java/com/example/myapplication/data/AllData.kt
@@ -1,5 +1,26 @@
 package com.example.myapplication.data
 
-//data class AllData(
-//
-//)
+import androidx.annotation.DrawableRes
+import com.example.myapplication.R
+
+data class WebToonData(
+    @DrawableRes val drawable: Int,
+)
+
+val RecentDataList = listOf(
+    R.drawable.example_image,
+    R.drawable.example_image,
+    R.drawable.example_image,
+    R.drawable.example_image,
+    R.drawable.example_image,
+    R.drawable.example_image,
+).map { WebToonData(it) }
+
+val PopDataList = listOf(
+    R.drawable.example_image2,
+    R.drawable.example_image2,
+    R.drawable.example_image2,
+    R.drawable.example_image2,
+    R.drawable.example_image2,
+    R.drawable.example_image2,
+).map { WebToonData(it) }
--- app/src/main/java/com/example/myapplication/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/myapplication/ui/screens/HomeScreen.kt
@@ -1,22 +1,69 @@
 package com.example.myapplication.ui.screens
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Scaffold
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.myapplication.R
+import com.example.myapplication.data.*
 
 @Composable
 fun HomeScreen() {
-//    Scaffold(
-////        content = HomeContent()
-//    )
-    Box(
-        contentAlignment = Alignment.Center,
-        modifier = Modifier.fillMaxSize()
+    Column {
+        Image(
+            painter = painterResource(id = R.drawable.firechicken),
+            contentDescription = null,
+            contentScale = ContentScale.Fit,
+            modifier = Modifier.size(393.dp)
+        )
+        Text(
+            text = "최근 본 웹툰 이어보기",
+            modifier = Modifier.padding(top = 10.dp, start = 16.dp)
+        )
+        WebToonGrid(
+            modifier = Modifier.padding(top = 6.dp),
+            items = RecentDataList,
+            width = 132, height = 82
+        )
+        Text(
+            text = "인기 판타지 웹툰",
+            modifier = Modifier.padding(top = 20.dp, start = 16.dp)
+        )
+        WebToonGrid(
+            modifier = Modifier.padding(top = 6.dp),
+            items = PopDataList,
+            width = 119, height = 165
+        )
+    }
+}
+
+// Card Tab
+
+@Composable
+fun WebToonGrid(
+    modifier: Modifier = Modifier,
+    items: List<WebToonData>,
+    width: Int,
+    height: Int
+) {
+    LazyRow(
+        modifier = modifier,
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
     ) {
-        Text(text = "This Screen is HomeScreen!!!")
+        items(items) { item ->
+            Image(
+                painter = painterResource(item.drawable),
+                contentDescription = null, // ?
+                contentScale = ContentScale.Fit,
+                modifier = Modifier.width(width.dp).height(height.dp)
+            )
+        }
     }
 }
